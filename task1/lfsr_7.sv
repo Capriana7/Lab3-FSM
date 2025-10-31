5756d5b19d64a3ac744de27edca7a41860eb8b2d
Ananya Gupta <ananyag987@example.com>
2025-10-31 22:42:23 +0000

    completed task1 challenge

--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -5,4 +5,14 @@ module lfsr_7 (
     output  logic [6:0] data_out
 );
 
+    logic [8:1] sreg; 
+
+    always_ff @(posedge clk or posedge rst)
+        if (rst)
+            sreg <= 4'b0001;
+        else if (en)
+            sreg <= {sreg[7:1], sreg[7] ^ sreg[3]};
+
+    assign data_out = sreg;
+
 endmodule
