2e17353071077a8a29559941fe0b0619c1e0c74b
Ananya Gupta <ananyag987@example.com>
2025-10-31 23:46:50 +0000

    completed task2

--- task2/f1_fsm.sv
+++ task2/f1_fsm.sv
@@ -5,4 +5,33 @@ module f1_fsm (
     output  logic [7:0] data_out
 );
 
+    typedef enum logic [3:0] {S0, S1, S2, S3, S4, S5, S6, S7, S8} my_state;
+    my_state current_state, next_state;
+
+    always_ff @(posedge clk)
+        if (rst) current_state <= S0;
+        else current_state <= next_state;
+            
+    always_comb begin
+        next_state = current_state; 
+        if (en) begin
+            case (current_state)
+                S0: next_state = S1;
+                S1: next_state = S2;
+                S2: next_state = S3;
+                S3: next_state = S4;
+                S4: next_state = S5;
+                S5: next_state = S6;
+                S6: next_state = S7;
+                S7: next_state = S8;
+                S8: next_state = S0; 
+                default: next_state = S0;
+            endcase
+        end
+    end
+
+
+    assign data_out = (8'b1 << current_state) - 1;
+
+
 endmodule
---
+++ task2/f1_fsm_tb.cpp
@@ -0,0 +1,45 @@
+#include "Vf1_fsm.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+
+    Vf1_fsm* top = new Vf1_fsm;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("f1_fsm.vcd");
+
+    if(vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 3: F1 FSM");
+    vbdSetMode(1); 
+
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    for(i=0; i<1000;i++){
+        for(clk=0; clk<2; clk++){
+            tfp->dump(2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        vbdCycle(i + 1);
+        
+        top->rst = (i<2) | (i==15) | (i==100);
+        top->en = vbdFlag();
+        vbdBar(top->data_out & 0xFF);
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
